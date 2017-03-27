cafac0a8bf4f150668a4dda265116473591e2c97
emma-supattapone <emmasupattapone@hanovernorwichschools.org>
2017-03-27 14:55:27 -0400

    Updated from Brython Server: 3/27/2017 2:55:26 PM

--- cryptography.py
+++ cryptography.py
@@ -10,37 +10,44 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-
-#x = 1
-#while x == 1: 
-#    ham = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-#    if ham == "e":
-#        mesg = input("Message: ")
-#        mesg = list(mesg)
-#        mesg = str(mesg)
-#        mesge = associations.find(mesg)
-#        print(mesge)
-#        kye = associations.find(ky)
-#        print(kye)
-#    elif ham == "d":
-#        mesgg = input("Message: ")
-#        kyy = input("Key: ")
-#        print("decrypt")
-#    elif ham == "q":
-#        print("Goodbye!")
-#        x = 2
-#    else:
-#        print("Did not understand command, try again.")
-
+"""
+x = 1
+while x == 1: 
+    ham = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if ham == "e":
+        mesg = input("Message: ")
+        mesg = list(mesg)
+        mesg = str(mesg)
+        mesge = associations.find(mesg)
+        print(mesge)
+        kye = associations.find(ky)
+        print(kye)
+    elif ham == "d":
+        mesgg = input("Message: ")
+        kyy = input("Key: ")
+        print("decrypt")
+    elif ham == "q":
+        print("Goodbye!")
+        x = 2
+    else:
+        print("Did not understand command, try again.")
+"""
 
 f = input("Messege: ")
 g = input("Key: ")
 f = list(f)
 g = list(g)
+#i = f.count
+#print(i)
+print(f)
+print(g)
+
 for h in f:
     f = associations.find(h)
 for e in g:
     g = associations.find(e)
-p = 
-p = associations.find(char)
-print(p)
+print(f)
+print(g)
+
+associations[index]
+#print(p)
